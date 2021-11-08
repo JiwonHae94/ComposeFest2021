b554f5e5530f729e9e65776be3e39b05e5d57130
JiwonHae <hjiwon1994@gmail.com>
2021-11-09 03:34:42 +0900

    - create scrollList

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/jiwon/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/jiwon/layoutscodelab/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jiwon.layoutscodelab.ui.theme.LayoutsCodelabTheme
+import com.jiwon.layoutscodelab.view.ImageList
+import com.jiwon.layoutscodelab.view.ScrollingList
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +54,7 @@ fun LayoutsCodeLab() {
         BodyContent(Modifier.padding(innerPadding))
     }
 }
+
 
 
 @Composable
