8ceb62d96b0760bb89a917c1fe6fdd2e7459937c
JiwonHae <hjiwon1994@gmail.com>
2021-11-09 01:27:00 +0900

    - create slotAPIs for slot api demonstration

---
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/jiwon/jetpackcomposelayouts/view/SlotAPIs.kt
@@ -0,0 +1,41 @@
+package com.jiwon.jetpackcomposelayouts.view
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+// Slot APIs
+@Composable
+fun SlotButton(
+    modifier : Modifier = Modifier,
+    text : String,
+    spacingBetweenIconAndText : Dp = 4.dp,
+    icon : Icon?,
+    onClick : (() -> Unit)? = null,
+    content : @Composable () -> Unit
+){
+
+}
+
+
+@Preview
+@Composable
+fun Button_Preview(){
+
+    SlotButton(
+        modifier = Modifier,
+        text = "Sample",
+        icon = null
+    ){
+        Row{
+            // TODO Something
+        }
+    }
+
+}
