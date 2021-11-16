8be624fc9d4c7dc3a24743f25e448baffd2c36ca
JiwonHae <hjiwon1994@gmail.com>
2021-11-17 04:35:27 +0900

    - create methods and variables to control stateful components used in the view

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
@@ -16,22 +16,51 @@
 
 package com.codelabs.state.todo
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
+    // remove the LiveData and replace it with a mutableStateListOf
+    //private var _todoItems = MutableLiveData(listOf<TodoItem>())
+    //val todoItems: LiveData<List<TodoItem>> = _todoItems
 
-    private var _todoItems = MutableLiveData(listOf<TodoItem>())
-    val todoItems: LiveData<List<TodoItem>> = _todoItems
+    private var currentEditPosition by mutableStateOf(-1)
 
-    fun addItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!! + listOf(item)
+    // By specifying private set, we're restricting writes to this state object to a
+    // private setter only visible inside the ViewModel.
+    var todoItems = mutableStateListOf<TodoItem>()
+        private set
+
+    // state
+    val currentEditItem : TodoItem?
+        get() = todoItems.get(currentEditPosition)
+
+    fun onEditItemSelected(item : TodoItem){
+        currentEditPosition = todoItems.indexOf(item)
     }
 
-    fun removeItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!!.toMutableList().also {
-            it.remove(item)
+    fun onEditDone(){
+        currentEditPosition = -1
+    }
+
+    fun onEditItemChange(item : TodoItem){
+        val currentItem = requireNotNull(currentEditItem)
+        require(currentItem.id == item.id){
+            "You can only change an item with the same id as currentEditItem"
         }
+
+        todoItems[currentEditPosition] = item
+    }
+
+    fun removeItem(item : TodoItem){
+        todoItems.remove(item)
+        onEditDone()
+    }
+
+    fun addItem(item: TodoItem) {
+        todoItems.add(item)
     }
 }
