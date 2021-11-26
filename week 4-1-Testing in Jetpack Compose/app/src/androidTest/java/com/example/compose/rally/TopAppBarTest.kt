13241cf0efd5d0370a5ff30c0dde928fd94e1a1c
JiwonHae <hjiwon1994@gmail.com>
2021-11-27 02:46:19 +0900

    TEST : merge semantics and apply multiple test rules for using .onNode(...)

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -50,10 +50,16 @@ class TopAppBarTest {
                 currentScreen = RallyScreen.Accounts)
         }
 
-        composeTestRule.onRoot().printToLog("currentLabelExists")
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 
